73fed3b3236c79404c105072b07e1d208ddfa402
AinoGG <ryadomgg@gmail.com>
2021-03-03 17:32:00 +0300

    sort is ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 import Loader from './Loader/Loader';
 import Table from './Table/Table';
+import _ from 'lodash';
 
 export default class App extends Component {
 
   state = {
     isLoading: true,
-    data: []
+    data: [],
+    sort: 'asc',
+    sortField: 'id'
   }
 
   async componentDidMount() {
     const response = await fetch(`http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`);
     const data = await response.json();
-    // console.log(data);
+   
     this.setState({
       isLoading: false,
-      data
+      data: _.orderBy(data, this.state.sortField, this.state.sort)
+    })
+  }
+
+  onSort = sortField => {
+
+    const cloneData = this.state.data.concat();
+    const sortType = this.state.sort === 'asc' ? 'desc' : 'asc';
+    const orderedData = _.orderBy(cloneData, sortField, sortType);
+
+    this.setState({
+      data: orderedData,
+      sort: sortType,
+      sortField
     })
   }
 
@@ -24,7 +40,13 @@ export default class App extends Component {
     return (
       <div className="container">
         {
-          this.state.isLoading ? <Loader /> : <Table data={this.state.data} />
+          this.state.isLoading
+            ? <Loader />
+            : <Table
+              data={this.state.data}
+              onSort={this.onSort}
+              sort={this.state.sort}
+              sortField={this.state.sortField} />
         }
 
       </div>
